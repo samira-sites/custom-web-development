0469d36bc72737b1d4d09be831ab9001f207798d
My laptop <samiraomar.xx@gmail.com>
2026-05-24 21:32:47 +0300

    modified index.php, add files  assets/icons/         manifest.json

--- index.php
+++ index.php
@@ -64,9 +64,9 @@
   <link rel="icon" href="/favicon.ico" type="image/x-icon">
   <link rel="icon" type="image/svg+xml" href="/favicon.svg">
   <link rel="icon" type="image/png" href="/favicon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="icon/32.png">
-  <link rel="icon" type="image/png" sizes="192x192" href="icon/192.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="icon/180.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/icons/icon_32.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="assets/icons/icon_192.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/icons/icon_180.png">
 
   <!-- PWA -->
   <link rel="manifest" href="manifest.json">
