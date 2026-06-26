1244d86c47d53a608d8ac75c16ba943b486a4b32
My laptop <samiraomar.xx@gmail.com>
2026-06-26 22:26:01 +0300

    updated

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL);
 require_once __DIR__ . '/../web/app/config.php';
 
 /* 2. Load Composer (PHPMailer + dotenv + everything) */
-require_once __DIR__ . '/../web/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 
 use PHPMailer\PHPMailer\PHPMailer;
