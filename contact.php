ca2fc9bda606a85c597a1d903a09b23d415b454a
My laptop <samiraomar.xx@gmail.com>
2026-06-26 20:01:59 +0300

    updated

--- contact.php
+++ contact.php
@@ -4,9 +4,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-/* --require_once __DIR__ . '/../web/app/config.php';-- */
+require_once __DIR__ . '/../web/app/config.php';
 
-require_once __DIR__ . '/../web/vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
