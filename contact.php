e93d00390eaa372aa9ed0918ccd56714fd9f6d33
My laptop <samiraomar.xx@gmail.com>
2026-06-27 11:23:54 +0300

    updated contact.php

--- contact.php
+++ contact.php
@@ -135,7 +135,7 @@ $mail->Debugoutput = 'html';
         $mail->addAddress($email, $name);
 
         $mail->isHTML(true);
-        $mail->Subject = "Your Message Has Been Received ⭐";
+        $mail->Subject = "Your Message Has Been Received";
 
         $safeName = htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
         $safeMessage = nl2br(htmlspecialchars($message, ENT_QUOTES, 'UTF-8'));
@@ -150,7 +150,7 @@ $mail->Debugoutput = 'html';
             <!-- Header -->
             <div style='background:linear-gradient(135deg,#fe8d14,#ffb347);padding:25px;text-align:center;color:#fff;box-shadow:0 0 10px'>
               <h2 style='margin:0;font-size:22px;'>Thank You for Reaching Out!</h2>
-              <p style='margin:5px 0 0;'>{$safeName}</p>
+              <p style='margin:5px 0 0;'>📩 {$safeName} 😊</p>
             </div>
         
             <!-- Body -->
